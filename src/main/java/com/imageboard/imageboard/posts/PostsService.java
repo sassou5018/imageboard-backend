9c8595996916f5a05b6b0c014f499b9ce06e0010
Yacinn <sassou.est100@gmail.com>
2022-12-15 23:38:14 +0100

    added services

--- src/main/java/com/imageboard/imageboard/posts/PostsService.java
+++ src/main/java/com/imageboard/imageboard/posts/PostsService.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostsService {
@@ -29,6 +30,10 @@ public class PostsService {
         return page.toList();
     }
 
+    public Long getPostsCount(){
+        return postsRepository.count();
+    }
+
     public Boolean verifyImageSrc(String url1) {
         try {
             Image image = ImageIO.read(new URL(url1));
@@ -36,5 +41,21 @@ public class PostsService {
         } catch (Exception e){
             return false;
         }
+    }
+
+    public Posts addNewPost(Posts post){
+        if(!this.verifyImageSrc(post.getImgSrc())){
+            throw new IllegalStateException("Link doesn't contain an image");
+        }
+        postsRepository.save(post);
+        return post;
+    }
+
+    public Optional<Posts> getPostById(Long id){
+        Optional<Posts> post = postsRepository.findById(id);
+        if(!post.isPresent()){
+            throw new IllegalStateException("No post with: " + id + " Exists");
+        }
+        return post;
     }
 }
